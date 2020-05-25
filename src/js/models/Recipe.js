47a77a0a2a5f2f24aa2788104672c516798ca828
Ivan Stevkovski <ivan@qbit.solutions>
2020-05-25 02:40:40 +0200

    Changed api and project is running again

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -8,13 +8,12 @@ export default class Recipe {
 
     async getRecipe() {
         try {
-            const res = await axios(`${proxy}http://food2fork.com/api/get?key=${key}&rId=${this.id}`);
+            const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
             this.title = res.data.recipe.title;
             this.author = res.data.recipe.publisher;
             this.img = res.data.recipe.image_url;
             this.url = res.data.recipe.source_url;
             this.ingredients = res.data.recipe.ingredients;
-            console.log(res);
         } catch (error) {
             console.log(error);
             alert('Something went wrong :(');
